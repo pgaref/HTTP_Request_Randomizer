0d7ea31df9610a8badbdba630b4579b152595540
Panagiotis Garefalakis <pangaref@gmail.com>
2016-10-31 23:05:53 +0000

    Include agents data file in the library distribution

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
+from glob import glob
 
-setup(name='http-request-randomizer',
-      version='0.0.3',
+setup(name='HTTP_Request_Randomizer',
+      version='0.0.4',
       description='A package using public proxies to randomise http requests.',
       url='https://pgaref.github.io/blog/python-proxy/',
       author='pgaref',
       author_email='pangaref@gmail.com',
       license='MIT',
       packages=find_packages(exclude=['tests']),
+      package_data={
+            # Include agents.txt files
+            'http.requests': ['data/*'],
+      },
+      include_package_data=True,
       install_requires=["requests", "bs4", "psutil", "httmock", "python-dateutil", "schedule"],
       zip_safe=False)
