a63ebaf8dd9b6c1fe655a996847f44e39a27aeca
pgaref <pgaref@macbook-air.lan>
2017-02-11 04:20:52 +0000

    Adding test for testing URL format - Periodic daily travis tests will reveal broken providers

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import unittest
 
 from http.requests.parsers.UrlParser import UrlParser
@@ -5,7 +7,7 @@ from http.requests.parsers.UrlParser import UrlParser
 __author__ = 'pgaref'
 
 
-class testBaseParser(unittest.TestCase):
+class TestBaseProxyParsers(unittest.TestCase):
     def setUp(self):
         self.normal_parser = UrlParser("http://proxy-test.com", bandwidthKBs=50)
         self.no_bdwidthParser = UrlParser("http://slow-proxy.com")
@@ -14,7 +16,7 @@ class testBaseParser(unittest.TestCase):
         self.assertEqual(self.normal_parser.get_URl(), "http://proxy-test.com", "incorrect parser URL")
         self.assertEqual(self.normal_parser.get_min_bandwidth(), 50, "incorrect parser bandwidth")
 
-    def test_no_bdwidth_parser(self):
+    def test_no_bandwidth_parser(self):
         self.assertEqual(self.no_bdwidthParser.get_URl(), "http://slow-proxy.com", "incorrect parser URL")
         self.assertEqual(self.no_bdwidthParser.get_min_bandwidth(), 150, "incorrect parser bandwidth")
 
---
+++ tests/test_providers.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+
+import unittest
+
+from http.requests.parsers.FreeProxyParser import FreeProxyParser
+from http.requests.parsers.ProxyForEuParser import ProxyForEuParser
+from http.requests.parsers.RebroWeeblyParser import RebroWeeblyParser
+from http.requests.parsers.SamairProxyParser import SamairProxyParser
+
+__author__ = 'pgaref'
+
+
+class TestProxyProviders(unittest.TestCase):
+    # def setUp(self):
+
+    def test_FreeProxyParser(self):
+        proxy_provider = FreeProxyParser('http://free-proxy-list.net')
+        proxy_provider.parse_proxyList()
+
+    def test_ProxyForEuParser(self):
+        proxy_provider = ProxyForEuParser('http://proxyfor.eu/geo.php')
+        proxy_provider.parse_proxyList()
+
+    def test_RebroWeeblyParser(self):
+        proxy_provider = RebroWeeblyParser('http://rebro.weebly.com')
+        proxy_provider.parse_proxyList()
+
+    def test_SemairProxyParser(self):
+        proxy_provider = SamairProxyParser('http://www.samair.ru/proxy/time-01.htm')
+        proxy_provider.parse_proxyList()
+
+
+if __name__ == '__main__':
+    unittest.main()
