73c3386dc592e2c9072ced290a8a5b997819243c
pgaref <pangaref@gmail.com>
2018-01-14 21:49:36 +0000

    Updated RDS endpoint + gitignore for elasticBeanstack

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://proxypad:caf39f756871906f@proxypad-rds.cmnnnyzv5ado.us-west-2.rds.amazonaws.com:3306/proxypaddb'
 
 # Uncomment the line below if you want to work with a local DB
-SQLALCHEMY_DATABASE_URI = 'sqlite:///proxypad-dev.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///proxypad-dev.db'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'proxypad-dev'
+SECRET_KEY = 'fcee20bce87dfe5a989bca6f57078c32168ff6775f39830d'
 DEBUG = True
